05b62d17af64260b89aed3d0f9b940dd56d98828
Sofiane <s-attilah@hotmail.com>
2020-02-19 16:17:03 +0100

    Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,48 +3,51 @@ import {useSpring, animated, config} from 'react-spring';
 import '../css/home.css';
 import homeDesktop from '../images/homeDesktop.jpg';
 import homeMobile from '../images/homeMobile.jpg';
-// import Map from './Map';
+import Map from './Map';
 
 const Home = (props) => {
-    // const [translateX, setTranslateX] = useState(0);
-    // const [x, setX] = useState(0);
+    const [translateX, setTranslateX] = useState(0);
+    const [x, setX] = useState(0);
     const [delay, setDelay] = useState(1500);
-    // const modelNames = ["Model S", "Model 3", "Model X"];
-    // const [n, setN] = useState(1);
+    const modelNames = ["Model S", "Model 3", "Model X"];
+    const [n, setN] = useState(1);
     let img = window.innerWidth > props.breakpoint ? homeDesktop : homeMobile;
-    let x = window.innerWidth > props.breakpoint ? 25 : 33;
+   
+    useEffect(() => {
+        window.innerWidth > props.breakpoint ? setX(25) : setX(33);
+    }, [x])
 
     const imgSpring = useSpring({
         config: config.molasses,
-        from: { transform: "scale(1)", transformOrigin: "50% 60%"},
-        to: {transform: window.innerWidth > props.breakpoint ? "scale(2)" : "scale(3)"},
+        from: { transform: `scale(1) translateX(0%)`, transformOrigin: "50% 60%"},
+        to: {transform: window.innerWidth > props.breakpoint ? `scale(2) translateX(${translateX}%)` : `scale(3) translateX(${translateX}%)`},
         delay: delay
     });
 
-    // const CarouselCaptionSpring = useSpring({config: {duration: 1000},from: {opacity: 0},to: {opacity: 1}, delay: 1500});
+    const CarouselCaptionSpring = useSpring({config: {duration: 1000},from: {opacity: 0},to: {opacity: 1}, delay: 1500});
    
-    // const translate = (e) => {
-    //     const id = e.target.id;
-    //     switch (id) {
-    //         case "prev":
-    //             setDelay(0)
-    //             translateX === -x ? setTranslateX(0) : setTranslateX(x);
-    //             translateX === -x ? setN(1) : setN(0);
-    //             break;
-    //         case "next":
-    //             setDelay(0)
-    //             translateX === x ? setTranslateX(0) : setTranslateX(-x);
-    //             translateX === x ? setN(1) : setN(2);
-    //             break;
-    //         default:
-    //             setTranslateX(0);
-    //             break;
-    //     }
-    // }
+    const translate = (e) => {
+        const id = e.target.id;
+        switch (id) {
+            case "prev":
+                setDelay(0)
+                translateX === -x ? setTranslateX(0) : setTranslateX(x);
+                translateX === -x ? setN(1) : setN(0);
+                break;
+            case "next":
+                setDelay(0)
+                translateX === x ? setTranslateX(0) : setTranslateX(-x);
+                translateX === x ? setN(1) : setN(2);
+                break;
+            default:
+                setTranslateX(0);
+                break;
+        }
+    }
     return (
         <div>
             <div className="carousel slide" data-ride="carousel">
-                {/* <ol className="carousel-indicators">
+                <ol className="carousel-indicators">
                     <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
                 </ol>
                 <div className="row position-absolute w-100 m-auto btn-row">
@@ -53,24 +56,24 @@ const Home = (props) => {
                         <button type="button" className="btn btn-outline-light m-2 btn-sm rounded-pill cta">EXISTING INVENTORY</button>
                         <a className="d-block text-white visit-store" href="#">Visit a store</a>
                     </animated.div>
-                </div> */}
+                </div>
                 <div className="carousel-inner" >
                     <div className="carousel-item active" style={{height: window.innerHeight, width: window.innerWidth }}>
                     <animated.img className="models" style={imgSpring} src={img} alt="models" />
-                    {/* <div className="carousel-caption d-md-block">
+                    <div className="carousel-caption d-md-block">
                         <animated.h5 className="modelsName" style={CarouselCaptionSpring}>{modelNames[n]}</animated.h5>
                         <animated.p className="modelsQuotes" style={CarouselCaptionSpring}>{translateX !== 0 && 'Free Unlimited Supercharging.'}</animated.p>
-                    </div> */}
+                    </div>
                     </div>
                 </div>
-                {/* <a className="carousel-control-prev mt-auto mb-auto h-25" role="button" id="prev" onClick={translate}>
+                <a className="carousel-control-prev mt-auto mb-auto h-25" role="button" id="prev" onClick={translate}>
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="sr-only">Previous</span>
                 </a>
                 <a className="carousel-control-next mt-auto mb-auto h-25"  role="button" id="next" onClick={translate}>
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="sr-only">Next</span>
-                </a> */}
+                </a>
             </div>
         </div>
     );
